8cfed9f0b3114f211b134accad559b1dc75dc209
skarlsson <svante.karlsson@csi.se>
2016-03-27 21:43:30 +0200

    update travis

--- travis.sh
+++ travis.sh
@@ -2,8 +2,17 @@
 set -evx
 env | sort
 
+wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download -Oboost_1_59_0.tar.gz
+tar xf boost_1_59_0.tar.gz
+
+cd boost_1_59_0
+if [ ! -f "./b2" ]; then ./bootstrap.sh; fi
+./b2 -j 4 link=static headers log_setup log date_time timer thread system program_options filesystem regex chrono 
+./b2 -j 4 link=static --with-log --with-thread --with-timer --with-program_options --with-iostreams
+cd ..
+
 mkdir build || true
 cd build
-cmake -DCMAKE_CXX_FLAGS=$CXX_FLAGS ..
+
+cmake -D__LINUX__=1 -DBoost_INCLUDE_DIRS=../boost_1_59_0 -DBoost_LIBRARY_DIRS=../boost_1_59_0/stage/lib -D__BUILD_EXAMPLES__=1 .. 
 make
-#make test
